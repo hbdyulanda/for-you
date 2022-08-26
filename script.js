cdd6109b42bb2d5ef057e3e188abf0dfc8b200b1
hbdyulanda <ylndgsmnr@gmail.com>
2022-08-26 18:17:43 +0700

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+var x = document.getElementById("myAudio");
+
+function playAudio() {
+    x.play();
+}
+
+const pages = document.querySelectorAll(".page");
+const translateAmount = 100;
+let translate = 0;
+
+slide = (direction) => {
+
+    direction === "next" ? translate -= translateAmount : translate += translateAmount;
+
+    pages.forEach(
+        pages => (pages.style.transform = `translateX(${translate}%)`)
+    );
+}
